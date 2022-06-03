f1c06ca979a134272f6d4e914fa6f9f1baa86590
John Santa <andersonsanta3@gmail.com>
2022-06-02 19:37:53 -0500

    Add example to template strings

---
+++ 02-intro-javascript/src/bases/02-template-string.js
@@ -0,0 +1,9 @@
+/**
+ * Template Strings
+ * Son usados para no hacer concatenaciones de strings.
+ */
+
+ const name = 'John';
+ const lastName = 'Santa';
+ 
+ console.log(`His name is ${name} ${lastName}`);
