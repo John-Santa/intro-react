369fd0d0dc65cb331c0f4b6f48d0abcc3242fb47
John Santa <andersonsanta3@gmail.com>
2022-07-11 18:43:40 -0500

    Example of functions

---
+++ 02-intro-javascript/src/bases/functions.js
@@ -0,0 +1,34 @@
+//* Functions
+//* No es recomedable creear este tipo de funciones ya que son facilmente modificables
+//* y puede generar errores
+function sayHello( name ){
+    return(`Hello ${name}`);
+}
+
+console.log(sayHello('John'));
+
+//* Arrow functions
+const sayHelloByName = (name) => {
+    return(`Hello ${name}`);
+}
+
+console.log(sayHelloByName('John'));
+
+const getUser = () => {
+    return{
+        uuid: '123',
+        name: 'John',
+        age: 30
+    }
+}
+
+console.log(getUser());
+
+const getActiveUser = (name) => ({
+        uuid: '123',
+        name: name,
+});
+
+
+const activeUser = getActiveUser('John');
+console.log(activeUser);
